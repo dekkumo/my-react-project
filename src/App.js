18c41525638a74766dd659ecdd46544a13f8ce72
dekkumo <houper8@gmail.com>
2022-12-28 11:03:54 +0300

    initial project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+function App() {
+
+ 
+
+  return (
+    <div className="App">
+      <input placeholder='new todo' />
+      <button>Add todo</button>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
