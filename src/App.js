156751661e8dafc4767216087953eca53a010cfb
dekkumo <houper8@gmail.com>
2022-12-29 19:38:38 +0300

    deleted todo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useRef, useState } from 'react';
 import './App.css';
 import { AddTodo } from './todo/AddTodo';
 import { ToDo } from './todo/ToDo';
-import { Button } from '@mui/material'
+import { Button, createTheme } from '@mui/material'
+import TextField from '@mui/material/TextField';
 
 function App() {
 
   const [todos, setTodos] = useState([])
   const textInput = useRef()
+  const todoText = useRef()
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -25,21 +27,45 @@ function App() {
 
     setTodos([...todos, todoObj])
   }
+
+  const handleClick = (id) => {
+    const newTodos = todos.filter((el) => {
+      return el.id !== id
+    })
+    setTodos(newTodos)
+  }
   
   return (
     <div className='App'>
       <form onSubmit={handleSubmit}>
         <input 
           required 
-          pattern="^[^\s]+(\s.*)?$" 
+          pattern="^[^\s]+(\s.*)?$"
           ref={textInput} 
           placeholder='new todo' 
         />
+
+        {/* <TextField
+          required 
+          pattern="^[^\s]+(\s.*)?$"
+          ref={textInput} 
+
+          style={{marginRight: 15}}
+          id="outlined-multiline-flexible"
+          label="new todo"
+          multiline
+          size='small'
+          sx={{width: 285}}
+          color='warning'
+        /> */}
         <Button type='submit' variant="contained">Add todo</Button>
       </form>
 
       {todos.map(el => (
-        <div className='todo' key={el.id}>{el.text}</div>
+        <div key={el.id} className='todo-list'>
+          <div>{el.text}</div>
+          <button onClick={() => handleClick(el.id)}>delete</button>
+        </div>
       ))}
     </div>
   );
